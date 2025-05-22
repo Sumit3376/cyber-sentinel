266e3a8ac3f34f8d68061faee2576b00ff43302f
Sumit Ranmale <sumitranmale3376@gmail.com>
2025-05-22 16:12:55 +0530

    Initial commit with project files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+   function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-white text-5xl font-bold">🚀 Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+  
+
+export default App;
